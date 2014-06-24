75ecf94292adece3c34a3b844987c45d648ca8bd
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-06-24 13:55:05 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Morgan Chen on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(id)init {
+    self = [super init];
+    
+    if(self){
+        CCLOG(@"Seal Created");
+    }
+    return self;
+}
+
+@end
