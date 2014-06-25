8097ea02bc83af73ff57092e8b5d8e0a2945b8aa
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-06-25 10:56:41 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,6 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
+    CCNode *_contentNode;
 }
 
 -(void)didLoadFromCCB {
@@ -35,7 +36,7 @@
     //have camera follow penguin
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 -(void)retry {
